9c4986500dd2f1b7fbcbb93eebfdc1eec0e9b269
Renan Prata <renancprata@gmail.com>
2023-09-18 14:55:39 -0300

    feat: updating main

--- main.go
+++ main.go
@@ -2,18 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 
-	"github.com/rprata/macgo/generator"
-	"github.com/rprata/macgo/vendor"
+	"github.com/rprata/macgo/internal/app"
 )
 
 func main() {
-	macAddress := generator.GenerateRandomMACAddress()
-	fmt.Println(macAddress)
-	fmt.Println(vendor.GetVendorInfo(macAddress))
-
-	fmt.Println(vendor.GetVendorInfo("98:CA:33:AA:BB:CC"))
-	fmt.Println(vendor.GetPrefixList("raspberry"))
-
-	fmt.Println(generator.GenerateRandomMACAddressByVendor("raspberry"))
+	app := app.Generate()
+	if error := app.Run(os.Args); error != nil {
+		fmt.Println(error)
+	}
 }
